9dab492e5dc0c0bbe5e221f6ace6a2ee921bd84b
irodotos <irodotos@vaslabs.io>
2024-06-13 15:24:30 +0300

    github ci

--- tests/use-exsisting-s3-bucket.tf
+++ tests/use-exsisting-s3-bucket.tf
@@ -1,17 +1,17 @@
-module "s3_bucket" {
+module "exsiting-s3_bucket" {
     source = "../s3_and_users_service"
     s3_bucket_name = "existing-s3-bucket"
-    environment = "dev"
-    project_name = "my-project"
+    environment = "exsisting-dev"
+    project_name = "exsisting-bucket-project"
     bucket_arn = "arn:aws:s3:::exsitings3bucket"
     user = {
         "irodotos": {
-            "iam": "irodotos.gitlab"
-            "keybase": "irodotos7"
+            "iam": "exsisting-irodotos.gitlab"
+            "keybase": "existing-irodotos7"
         },
         "vasilis": {
-            "iam": "vasilis.gitlab"
-            "keybase": "vasilis7"
+            "iam": "exsisting-vasilis.gitlab"
+            "keybase": "exsisting-vasilis7"
         }
     }
 }
