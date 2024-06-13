d8b01ccd62ac1a6223e257c36f47c94efa272d52
irodotos <irodotos@vaslabs.io>
2024-06-13 15:50:27 +0300

    add pre-commit scripts

---
+++ generate_docs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+generate_docs_in_readme() {
+    module=$1
+    cd $module
+    terraform-docs markdown . --output-file README.md
+    cd -
+}
+
+for module in $(ls -d lambda_*); do
+    terraform fmt
+    generate_docs_in_readme $module
+done
+
+# terraform fmt sqs_to_sns
+# terraform fmt s3_to_sqs
+# terraform fmt tests
+
+
+# generate_docs_in_readme sqs_to_sns
+# generate_docs_in_readme s3_to_sqs
