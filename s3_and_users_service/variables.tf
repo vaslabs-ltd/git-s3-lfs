ef978dffaa611a9fd847569883308190bef11653
irodotos <irodotos@vaslabs.io>
2024-06-14 12:34:33 +0300

    better descriptions for the variables

--- s3_and_users_service/variables.tf
+++ s3_and_users_service/variables.tf
@@ -22,44 +22,46 @@ variable "project_name" {
 
 variable "minimum_password_length" {
   type        = number
-  description = "The minimum lenght of the password."
+  description = "The minimum lenght of the aws account password."
   default     = 16
 }
 
 variable "require_lowercase_characters" {
   type        = bool
-  description = "The password requires lowercase characters."
+  description = "The aws account password requires lowercase characters."
   default     = true
 }
 
 variable "require_numbers" {
   type        = bool
-  description = "The password requires numbers."
+  description = "The aws account password requires numbers."
   default     = true
 }
 
 variable "require_uppercase_characters" {
   type        = bool
-  description = "The password requires uppercase characters."
+  description = "The aws account password requires uppercase characters."
   default     = true
 }
 
 variable "require_symbols" {
   type        = bool
-  description = "The password requires symbols."
+  description = "The aws account password requires symbols."
   default     = true
 }
 
 variable "allow_users_to_change_password" {
   type        = bool
-  description = "Allow users to change the password ."
+  description = "Allow users to change the aws account password ."
   default     = true
 }
 
 variable "user" {
   type = map(map(string))
+  description = "The users that will be created. Need to pass the aws account name and the keybase username. Check the /examples"
 }
 
 variable "tags" {
   type = map(string)
+  description = "The tags that will be added to the resources."
 }
