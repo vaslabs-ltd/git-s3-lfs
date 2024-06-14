adbf824919c355e19df8d44a6fd5626b0ec1e45c
irodotos <irodotos@vaslabs.io>
2024-06-14 11:16:19 +0300

    change pipelone tf root

---
+++ examples/shared.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "eu-west-1"
+
+  /*only for testing*/
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
